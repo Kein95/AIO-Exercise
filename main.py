23046b134618ba326cea43e3cd5c5f82d89b025e
AIO - 005 <htkien95@gmail.com>
2024-07-01 17:19:09 +0700

    Initial commit of Week 4 project files

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title('My project')
+st.header('This is my header')
+st.subheader('This is a subheader')
+
+st.divider()
+st.header('This is a header')
+st.subheader('This is a subheader')
+st.text('AI VIETNAM')
+st.text('ALO')
+
+st.divider()
