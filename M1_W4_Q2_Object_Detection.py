a7dbe1198202105a59fe3a11f14f1c738d7a5a57
AIO - 005 <htkien95@gmail.com>
2024-07-01 21:43:00 +0700

    Fixed bug in the authentication module

---
+++ M1_W4_Q2_Object_Detection.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+MODEL = "source/source/model/MobileNetSSD_deploy.caffemodel"
+PROTOTXT = "source/source/model/MobileNetSSD_deploy.prototxt.txt"
+
+def process_image(image):
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net = cv2.dnn.readNetFromCaffe(PROTOTXT, MODEL)
+    net.setInput(blob)
+    detections = net.forward()
+    return detections
+
+def annotate_image(
+        image, detections, confidence_threshold=0.5
+    ):
+    # loop over the detections
+    (h, w) = image.shape[:2]
+    for i in np.arange(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > confidence_threshold:
+            # compute the (x, y)-coordinates of the bounding box for the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            cv2.rectangle(image, (startX, startY), (endX, endY), 70, 2)
+    return image
+
+def main():
+    st.title('Object Detection for Images')
+    file = st.file_uploader('Upload Image', type=['jpg', 'png', 'jpeg'])
+    if file is not None:
+        st.image(file, caption="Uploaded Image")
+
+        image = Image.open(file)
+        image = np.array(image)
+        detections = process_image(image)
+        processed_image = annotate_image(image, detections)
+        st.image(processed_image, caption="Processed Image")
+
+if __name__ == "__main__":
+    main()
+
+# streamlit run "D:\00 AIO\AIO GIT HUB\AIO-Exercise\Module 1\Week 4\M1_W4_Q2_Object_Detection.py"
